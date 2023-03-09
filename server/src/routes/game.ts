2b4a1471fddd2ae56e20dd3a21ef491b3c1a31a1
Lucas Karlsson <lucas.karlsson@post.com>
2023-03-09 15:41:29 +0100

    added endpoints for information

---
+++ server/src/routes/game.ts
@@ -0,0 +1,40 @@
+import { Router, Request, Response } from "express";
+
+import {
+  getCategories,
+  CategoryType,
+  getQuestionsCountByCategory,
+  QuestionsCountType,
+  QuestionsCountGlobalType,
+  getQuestionsCountGlobal,
+} from "../api/triviaAPI";
+
+export const router = Router();
+
+router.get("/game/info/categories", async (_, res: Response) => {
+  try {
+    const categories: CategoryType[] = await getCategories();
+    res.send(categories);
+  } catch {
+    res.status(500).send();
+  }
+});
+
+router.get("/game/info/questions", async (_, res: Response) => {
+  try {
+    const count: QuestionsCountGlobalType = await getQuestionsCountGlobal();
+    res.send(count);
+  } catch {
+    res.status(500).send();
+  }
+});
+
+router.get("/game/info/questions/:id", async (req: Request, res: Response) => {
+  try {
+    const id: number = +req.params.id;
+    const count: QuestionsCountType = await getQuestionsCountByCategory(id);
+    res.send(count);
+  } catch {
+    res.status(400).send();
+  }
+});
