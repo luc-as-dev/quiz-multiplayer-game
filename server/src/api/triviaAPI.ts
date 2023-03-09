dfd40b455e95297e406c3249bb85f7a2b7b42f37
Lucas Karlsson <lucas.karlsson@post.com>
2023-03-09 16:40:24 +0100

    added method for fetching questions

--- server/src/api/triviaAPI.ts
+++ server/src/api/triviaAPI.ts
@@ -1,7 +1,24 @@
 const API = {
+  questions: "https://opentdb.com/api.php",
   categories: "https://opentdb.com/api_category.php",
   questionsCount: "https://opentdb.com/api_count.php?category=",
   questionsCountGlobal: "https://opentdb.com/api_count_global.php",
+};
+
+export type QuestionParamsType = {
+  amount?: number;
+  category?: number | "any";
+  difficulty?: "easy" | "medium" | "hard" | "any";
+  type?: "boolean" | "multiple" | "any";
+};
+
+export type QuestionType = {
+  category: string;
+  type: string;
+  difficulty: string;
+  question: string;
+  correct_answer: string;
+  incorrect_answers: string[];
 };
 
 export type CategoryType = {
@@ -36,9 +53,35 @@ export type QuestionsCountGlobalType = {
   };
 };
 
+type QuestionsDataType = {
+  response_code: number;
+  results: QuestionType[];
+};
+
 type CategoriesDataType = {
   trivia_categories: CategoryType[];
 };
+
+export async function getQuestions({
+  amount,
+  category,
+  difficulty,
+  type,
+}: QuestionParamsType) {
+  let queriesStr: string = `?amount=${amount}`;
+  if (category !== "any") {
+    queriesStr = queriesStr + `&category=${category}`;
+  }
+  if (difficulty !== "any") {
+    queriesStr = queriesStr + `&difficulty=${difficulty}`;
+  }
+  if (type !== "any") {
+    queriesStr = queriesStr + `&type=${type}`;
+  }
+  const response: Response = await fetch(API.questions + queriesStr);
+  const data: QuestionsDataType = await response.json();
+  return data.results;
+}
 
 export async function getCategories() {
   const response: Response = await fetch(API.categories);
