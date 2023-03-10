f65791b4ee910c21ab273fd19c2fcc230cee0325
Lucas Karlsson <lucas.karlsson@post.com>
2023-03-10 18:44:27 +0100

    added onClick

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,10 +1,20 @@
-import React, { ReactNode, useState } from "react";
+import React, { MouseEventHandler, ReactNode, useState } from "react";
+import NewGame from "../views/NewGame";
 
 type Props = {
-  className?: string;
-  children?: ReactNode;
+  className?: string | undefined;
+  onClick?: MouseEventHandler<HTMLDivElement> | undefined;
+  children?: ReactNode | undefined;
 };
 
-export default function Card({ className, children }: Props) {
-  return <div className={`card ${className || ""}`}>{children}</div>;
+export default function Card({ className, onClick, children }: Props) {
+  return (
+    <div
+      className={`card ${className || ""}`}
+      onClick={onClick ? onClick : undefined}
+      style={onClick ? { cursor: "pointer" } : {}}
+    >
+      {children}
+    </div>
+  );
 }
