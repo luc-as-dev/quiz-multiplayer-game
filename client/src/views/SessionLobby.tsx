a01a2c7612019b71a2494450c18d6b5f1d1e49c6
Lucas Karlsson <lucas.karlsson@post.com>
2023-03-11 11:23:16 +0100

    added basic SessionLobby

---
+++ client/src/views/SessionLobby.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { SessionContextType } from "../@types/Session";
+import { ViewContextType } from "../@types/View";
+import Card from "../components/Card";
+import { useSessionContext } from "../context/SessionContext";
+import { useViewContext } from "../context/ViewContext";
+import Home from "./Home";
+
+type Props = {};
+
+export default function SessionLobby({}: Props) {
+  const { setView }: ViewContextType = useViewContext();
+  const { getSession, getPlayers }: SessionContextType = useSessionContext();
+
+  function startGameHandler() {}
+
+  function leaveSessionHandler() {
+    setView(<Home />);
+  }
+
+  return (
+    <div className="view">
+      <Card className="grow-10">
+        <p className="medium-text">Session {getSession()!.id}</p>
+        <div className="flex-wrap">
+          <p>Deltagare:</p>
+          {getPlayers().map((player) => (
+            <p key={player}>{player}</p>
+          ))}
+        </div>
+      </Card>
+      <Card className="big-text grow-1" onClick={startGameHandler}>
+        Starta
+      </Card>
+      <Card className="big-text grow-1" onClick={leaveSessionHandler}>
+        Tillbaka
+      </Card>
+    </div>
+  );
+}
