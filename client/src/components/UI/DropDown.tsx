f637567101a7fc97b55a192154553239fd49d3f4
Lucas Karlsson <lucas.karlsson@post.com>
2023-03-15 23:24:44 +0100

    added DropDown-component to SessionLobby

---
+++ client/src/components/UI/DropDown.tsx
@@ -0,0 +1,40 @@
+import React, { ReactNode, useState } from "react";
+
+type Props = {
+  selected: string;
+  alternatives: string[];
+  setSelected: (name: string) => void;
+};
+
+export default function DropDown({
+  selected,
+  alternatives,
+  setSelected,
+}: Props) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  function openHandler() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
+  function selectHandler(name: string) {
+    setSelected(name);
+  }
+
+  return (
+    <div className={`drop-down ${isOpen ? "open" : ""}`} onClick={openHandler}>
+      <div className="d-head">{selected}</div>
+      <div className={"d-list"}>
+        {alternatives.map((name) => (
+          <div
+            className={`d-item ${name === selected ? "selected" : ""}`}
+            onClick={() => selectHandler(name)}
+            key={name}
+          >
+            {name}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- client/src/views/SessionLobby.tsx
+++ client/src/views/SessionLobby.tsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ILibrary } from "../@types/QuizClient";
 import { SessionContextType } from "../@types/Session";
 import Card from "../components/UI/Card";
+import DropDown from "../components/UI/DropDown";
 import useSession from "../hooks/useSession";
 
 type Props = {};
 
 export default function SessionLobby({}: Props) {
   const session: SessionContextType = useSession();
+
+  const [libraries, setLibraries] = useState<string[]>([]);
+  const [selectedLibrary, setSelectedLibrary] = useState<string>("");
+  const [library, setLibrary] = useState<ILibrary | null>(null);
+  const [selectedCategory, setSelectedCategory] = useState<string>("");
+  const [selectedDifficulty, setSelectedDifficulty] = useState<string>("");
+
+  useEffect(() => {
+    async function loadLibraries() {
+      const libraries = await session.getLibraries();
+      setLibraries(libraries);
+      setSelectedLibrary(libraries[0]);
+    }
+    loadLibraries();
+  }, []);
+
+  useEffect(() => {
+    async function loadLibrary(name: string) {
+      const library = await session.getLibrary(name);
+      if (library) {
+        setLibrary(library);
+        setSelectedCategory(library.categories[0]);
+        setSelectedDifficulty(library.difficulties[0]);
+      }
+    }
+    if (selectedLibrary !== "") loadLibrary(selectedLibrary);
+  }, [selectedLibrary]);
 
   function startGameHandler() {
     if (session.hasSession() && session.getIsOwner()) {
@@ -28,6 +57,27 @@ export default function SessionLobby({}: Props) {
             <p key={player}>{player}</p>
           ))}
         </div>
+
+        <DropDown
+          selected={selectedLibrary || ""}
+          alternatives={libraries}
+          setSelected={setSelectedLibrary}
+        />
+
+        {library && (
+          <DropDown
+            selected={selectedCategory}
+            alternatives={library?.categories}
+            setSelected={setSelectedCategory}
+          />
+        )}
+        {library && (
+          <DropDown
+            selected={selectedDifficulty}
+            alternatives={library?.difficulties}
+            setSelected={setSelectedDifficulty}
+          />
+        )}
       </Card>
       {session.getIsOwner() && (
         <Card className="big-text grow-1" onClick={startGameHandler}>
